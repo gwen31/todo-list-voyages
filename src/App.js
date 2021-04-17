c234ee22dd0b398399555a43a2ea07631bcc1c9f
gwenaelle_merle <gwenaelle-merle_student2021@wilder.school>
2021-04-17 18:27:17 +0200

    composant gard

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ListGard from "./components/Listgard";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <ListGard/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/listgard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function ListGard(){
+    return(
+        <div>
+            <h1>Visite à Faire dans le GARD</h1>
+
+
+        </div>
+
+    );
+}
+export default ListGard;
